acce553eba6d0a8e4e7488fffb732c3b05271c18
222lore <lorenzotkwenda@gmail.com>
2022-12-02 02:14:32 -0600

    no txt ok

--- day2.py
+++ day2.py
@@ -16,16 +16,12 @@ def part1(file):
 def part2(file):
     line = file.readline()
     result = [0, 3, 6]
+    play = [[3, 1, 2], [1, 2, 3], [2, 3, 1]]
     score = 0
 
     while line != "":
         score += result[ord(line[2:3]) - 88]        
-        if line[0:1] == 'X':
-            score += 4
-        elif line[2:3] == 'Y':
-            score += 1
-        else:
-            score += 7
+        score += play[ord(line[2:3]) - 88][ord(line[0:1]) - 65]
 
         line = file.readline()
 
@@ -33,21 +29,10 @@ def part2(file):
 
 def main():
     file = open("day2.txt", "r")
+    file2 = open("day2.txt", "r")
     
     print(part1(file))
-    # print("ascii " , ord('A') , " " , ord('B') , " " , ord('C') , " " , ord('X') , " " , ord('Y') , " " , ord('Z'))
+    print(part2(file2))
 
 
 if __name__ == "__main__": main()
-
-#rock A 65 - X 88
-#paper B 66 - Y 89
-#scissors C 67 - Z 90
-
-#           rock (88)    paper (89)   scissors (90) 
-# rock (65)     23 T          24 W              25 L
-# paper (66)    22 L          23 T              24 W
-# scissors (67) 21 W          22 L              23 T
-#lose = 0 (22 22 22)
-#tie = 3 (23 23 23)
-#win = 6 (24 24 24)
